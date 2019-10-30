c80d4d2dba5ee025d190e477e23e80477b393b62
Brooke <brookepaglia@gmail.com>
2019-10-30 18:18:11 -0400

    Add GUI gameplay functionality

--- main.js
+++ main.js
@@ -1,61 +1,47 @@
 const options = ["rock", "paper", "scissors"];
-    let points = {player: 0, computer: 0};
-    
-    function computerPlay() {
-      return options[Math.floor(Math.random() * options.length)]
-    }
+let points = {player: 0, computer: 0};
+let playerScore = document.getElementById('player-score');
+let computerScore = document.getElementById('computer-score');
 
-    function playerSelection() {
-      let ans = prompt("Choose one: rock, paper, or scissors");
-      return options.includes(ans.toLowerCase()) ? ans : playerSelection();
-    }
+function init() {
+  playerScore.textContent = `${points.player}`
+  computerScore.textContent = `${points.computer}`
+}
 
-    function playRound(playerChoice, computerSelection) {
-      playerChoice = playerSelection();
-      computerSelection = computerPlay();
-      
-      console.log("Player chooses " + playerChoice + " and Computer chooses " + computerSelection + "!");
-      switch (playerChoice) {
-        case "rock":
-          if (computerSelection === "paper") {
-            score("computer");
-          } else if (computerSelection === "scissors") {
-            score("player");
-          } else {
-            console.log("It's a tie! No points awarded.");
-          };
-          break;
-        case "paper":
-          if (computerSelection === "scissors") {
-            score("computer");
-          } else if (computerSelection === "rock") {
-            score("player");
-          } else {
-            console.log("It's a tie! No points awarded.");
-          };
-          break;
-        case "scissors":
-          if (computerSelection === "rock") {
-            score("computer");
-          } else if (computerSelection === "paper") {
-            score("player");
-          } else {
-            console.log("It's a tie! No points awarded.");
-          };
-          break;          
-      }
-    }
+function computerPlay() {
+  return options[Math.floor(Math.random() * options.length)]
+}
 
-    function score(winner) {
-      winner == "player" ? points.player++ : points.computer++;
-      console.log("Player: " + points.player + "\nComputer: " + points.computer)
-    }
+//function playerSelection() {
+//  let ans = prompt("Choose one: rock, paper, or scissors");
+// return options.includes(ans.toLowerCase()) ? ans : playerSelection();
+//}
 
-    function game() {
-      // do while score <= 5
-      // offer to reset at 5 points
-      do {
-        playRound();
-      } while (!(points.player >= 5 || points.computer >= 5))
-      points.player == 5 ? console.log("Game over! Player wins!") : console.log("Game over! Computer wins!")
-    }
+function playRound() {
+  playerChoice = event.target.id;
+  let computerSelection = computerPlay();
+  if (playerChoice === computerSelection) {
+    score("tie");
+  } else if ((playerChoice === "rock" && computerSelection === "scissors") || (playerChoice === "paper" && computerSelection === "rock") || (playerChoice === "scissors" && computerSelection === "paper")) {
+    score("player");
+  } else {
+    score("computer");
+  }
+}
+
+function score(winner) {
+  winner == "player" ? points.player++ : points.computer++;
+  playerScore.textContent = `${points.player}`
+  computerScore.textContent = `${points.computer}`
+}
+
+function game() {
+  // do while score <= 5
+  // offer to reset at 5 points
+  do {
+    playRound();
+  } while (!(points.player >= 5 || points.computer >= 5))
+  points.player == 5 ? console.log("Game over! Player wins!") : console.log("Game over! Computer wins!")
+}
+
+init();
